0b73fb526c908c8d2db7ef698c76283ab671b378
Shynee1 <jackbarkerford@gmail.com>
2023-12-22 14:34:26 -0500

    Basic bug fixes

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ import os
 
 subreddit = "amitheasshole"
 category = "top"
-system_path = "C:/Program Files/TikTok Video Creator"
 background_video_path = "assets/background_videos/"
 background_audio_path = "assets/background_audios/"
 output_video_path = "outputs/videos/"
 output_audio_path = "outputs/audios/"
+iterations = 2
 
 # Check if output directories are missing
 if not os.path.exists(output_video_path) or not os.path.exists(output_audio_path):
@@ -21,17 +21,16 @@ if not os.path.exists(output_video_path) or not os.path.exists(output_audio_path
 if not os.path.exists(background_video_path) or not os.path.exists(background_audio_path):
     print(f"ERROR: Missing directory '{background_video_path}' or '{background_audio_path}'")
     
-iterations = 0
 post_grabber = PostGrabber(subreddit, category)
 tts = TikTokTTS("en_us_006")
 
-while iterations <= 2:
+for i in range(1, iterations):
     # Scrape subreddit and obtain post text
     post = post_grabber.next_post()
     text = post_grabber.get_post_text(post)
     
-    audio_destination = output_audio_path + "audio" + str(iterations) + ".mp3"
-    video_desination = output_video_path + "final" + str(iterations) + ".mp4"
+    audio_destination = output_audio_path + "audio" + str(i) + ".mp3"
+    video_desination = output_video_path + "final" + str(i) + ".mp4"
 
     # Generate TTS using TikTok's text-to-speech
     tts.create_tts(text, audio_destination)
@@ -44,6 +43,5 @@ while iterations <= 2:
 
     creator.free_memory()
 
-    print(f'Iteration {iterations} completed')
-    iterations += 1
+    print(f'Iteration {i} completed')
 
